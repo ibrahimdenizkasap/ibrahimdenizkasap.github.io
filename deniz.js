fc1f7116a82552d68ae345a7e40c13276dee023a
ibrahimdenizkasap <deniz.ksp@gmail.com>
2023-10-27 17:01:22 +0300

    mobile navigation button added

--- deniz.js
+++ deniz.js
@@ -1,14 +1,44 @@
+function closeNav() {
+  document.getElementById("navbar").style.top = "-100%";
+}
+
+//
+
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-200px";
+    document.getElementById("navbar").style.top = "-100%";
   }
   prevScrollpos = currentScrollPos;
 }
 
-function closeNav() {
-  document.getElementById("navbar").style.top = "-200px";
+//
+
+function openNav() {
+  document.getElementById("mobilemenu").style.right = "0"
 }
+
+function closeMenu() {
+  document.getElementById("mobilemenu").style.right = "-100%"
+}
+
+//
+
+// document.addEventListener('click', function(e){   
+//   if (document.getElementById('mobilemenu').contains(e.target)){
+//     document.getElementById("mobilemenu").style.right = "0"
+//   } else{
+//     document.getElementById("mobilemenu").style.right = "-100%";
+//   }
+// });
+
+// document.addEventListener('click', function handleClickOutsideBox(event) {
+//   const box = document.getElementById('mmenu');
+
+//   if (!box.contains(event.target)) {
+//     box.style.display = 'none';
+//   }
+// });
